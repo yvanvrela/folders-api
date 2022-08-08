e38b1651af0d4b675cbed69459c5a48d029fefc8
Yvan <varelaivan30@gmail.com>
2022-08-08 00:24:53 -0400

    Add contribuyente blueprint

--- src/extensions/routes_extension.py
+++ src/extensions/routes_extension.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from app.routes.folders_route import folder
+from app.routes.contribuyentes_route import contribuyente
 
 
 def register_routes(app: Flask) -> None:
     app.register_blueprint(folder, url_prefix='/api/folders')
+    app.register_blueprint(contribuyente, url_prefix='/api/contribuyentes')
