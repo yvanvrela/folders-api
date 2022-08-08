257b18ff55326f68b4ba9fce75cf66963b8325b3
Yvan <varelaivan30@gmail.com>
2022-08-08 00:18:33 -0400

    Add database uri

--- src/config/dev_config.py
+++ src/config/dev_config.py
@@ -1,8 +1,9 @@
 from os import getenv
-
 from .base_config import BaseConfig
 
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
     TESTING = False
+    SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
