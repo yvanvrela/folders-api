b282f8e62610c093868c8a2e68ec5e93dac31d0a
Yvan <varelaivan30@gmail.com>
2022-08-10 13:57:04 -0400

    add get_forlders

--- src/app/routes/folders_route.py
+++ src/app/routes/folders_route.py
@@ -1,10 +1,23 @@
 from flask import Blueprint, jsonify
+from database.models.folder_model import FolderModel
+from database.schemas.folder_schema import FolderShema
+from extensions.database_extension import db
 
 folder = Blueprint('api_folder', __name__)
+
+folder_schema = FolderShema()
+folders_schema = FolderShema(many=True)
+
 
 @folder.route('/', methods=['GET'])
 def get_folders():
     try:
-        return jsonify({'message':'All folders'})
+        all_folders = FolderModel.query.all()
+
+        result = folders_schema.dump(all_folders)
+
+        response = result if [] else {'message': 'Not folders'}
+
+        return jsonify(response), 200
     except:
-        return jsonify({'message':'Error folders'})
+        return jsonify({'message': 'Error'})
