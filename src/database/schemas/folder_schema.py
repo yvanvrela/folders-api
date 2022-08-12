08c4c79b3ec3f48303b955f12c8db1b36c1cfa0f
Yvan Varela <varelaivan30@gmail.com>
2022-08-12 18:34:49 -0400

    Add required params

--- src/database/schemas/folder_schema.py
+++ src/database/schemas/folder_schema.py
@@ -1,6 +1,12 @@
 from extensions.database_extension import ma
+from marshmallow import Schema, fields, validate, ValidationError
 
 
 class FolderShema(ma.Schema):
+    id = fields.Integer()
+    contribuyente_id = fields.Integer(required=True)
+    color = fields.String(required=True)
+    time = fields.String(required=True)
+
     class Meta:
         fields = ('id', 'contribuyente_id', 'color', 'time')
