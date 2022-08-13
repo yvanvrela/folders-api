cccb6cf2404083d513d7ebacba287931dfdac02f
Yvan Varela <varelaivan30@gmail.com>
2022-08-13 02:39:52 -0400

    Add migration

--- src/extensions/database_extension.py
+++ src/extensions/database_extension.py
@@ -1,17 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
 
 db = SQLAlchemy()
 ma = Marshmallow()
+migrate = Migrate()
 
 
 def register_database_config(app: Flask):
     db.init_app(app)
     ma.init_app(app)
-
-    with app.app_context():
-        from database.models.contribuyente_model import ContribuyenteModel
-        from database.models.folder_model import FolderModel
-        db.create_all()
-        db.session.commit()
+    from database.models.contribuyente_model import ContribuyenteModel
+    from database.models.folder_model import FolderModel
+    migrate.init_app(app, db, directory='src/database/migrations')
