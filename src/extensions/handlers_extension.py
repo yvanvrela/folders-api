b03750e1c4449cbdf48b93b1695abb3037c87400
Yvan Varela <varelaivan30@gmail.com>
2022-08-12 21:43:50 -0400

    Add 400 error

--- src/extensions/handlers_extension.py
+++ src/extensions/handlers_extension.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from app.handlers.errors_handler import page_not_found
+from app.handlers.errors_handler import bad_request, page_not_found
 
 
 def register_handlers(app: Flask) -> None:
     app.register_error_handler(404, page_not_found)
+    app.register_error_handler(400, bad_request)
