b9114a6134e2fde519e29ec46d1d1cbc4182194a
Yvan Varela <varelaivan30@gmail.com>
2022-08-15 00:51:28 -0400

    Add fields required

--- src/database/schemas/contribuyente_schema.py
+++ src/database/schemas/contribuyente_schema.py
@@ -1,6 +1,11 @@
 from extensions.database_extension import ma
+from marshmallow import fields, ValidationError
 
 
 class ContribuyenteSchema(ma.Schema):
+    id = fields.Integer()
+    name = fields.String(required=True)
+    ruc = fields.String(required=True)
+
     class Meta:
         fields = ('id', 'name', 'ruc')
